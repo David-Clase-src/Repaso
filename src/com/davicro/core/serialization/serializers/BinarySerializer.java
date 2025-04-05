bf7446aa219262fe46dace90bc144d11a17c71bf
David-Clase-src <david.rollonla.clase@gmail.com>
2025-04-05 21:41:36 +0200

    nothing

--- src/com/davicro/core/serialization/serializers/BinarySerializer.java
+++ src/com/davicro/core/serialization/serializers/BinarySerializer.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.List;
 
 import com.davicro.core.serialization.DeserializationException;
 import com.davicro.core.serialization.ISerializer;
